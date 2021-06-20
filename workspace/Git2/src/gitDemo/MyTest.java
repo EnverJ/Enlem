094c24fbaf7041904980092b4b773209bfbade42
EnverJ <Enlem1987@gmail.com>
2021-06-20 13:26:03 -0600

    Enlem One

---
+++ workspace/Git2/src/gitDemo/MyTest.java
@@ -0,0 +1,13 @@
+package gitDemo;
+
+public class MyTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a =10;
+if(a>5){
+	System.out.println("Valid Amount");
+}
+	}
+
+}
